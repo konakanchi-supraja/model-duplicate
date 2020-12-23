c54700cae93c00f396fe11e9b90ece0f185e89f3
Konakanchi Lakshmi Supraja <59195095+konakanchi-supraja@users.noreply.github.com>
2020-12-23 23:52:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import TodoApp from "./TodoApp"
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Todo list</span><br />
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
